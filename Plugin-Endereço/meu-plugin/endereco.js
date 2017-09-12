1f9f091470e5b4b94111607a6004f9c541c957b0
ViniciusMoreira23 <vini.08.nf@hotmail.com>
2017-09-12 17:49:06 -0300

    12-09

---
+++ Plugin-Endereço/meu-plugin/endereco.js
@@ -0,0 +1,12 @@
+//;var plugin = function($){
+//		$.fn.verificador = function(tamanho){
+//		
+//		
+//            
+//            $("#digita-cep").append($("<input />")).append($("<buttom />"));
+//
+//		var $input = $(this).children("input");
+//		var $buttom = $(this).children("buttom");
+//        }};
+//
+//;
